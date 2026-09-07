9fb9c885dd091d9fd21e00114b6dd05486538442
dengsiqiang <3517267757@qq.com>
2026-09-07 13:36:10 +0800

    Initial commit

---
+++ untitled1/src/main/java/org/example/test1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Set;
+
+public class test1 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        for (int j : arr) {
+            System.out.print(j);
+        }
+        System.out.println(arr.length);
+        System.out.println("Hello World!!!");
+    }
+}
